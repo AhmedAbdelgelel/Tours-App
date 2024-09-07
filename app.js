1ebde5b5e5b1e72ead971f4666b15110b9eeff1e
Ahmed Abdelglel <pokelokeyoke1g@gmail.com>
2024-09-07 19:38:59 +0300

    setting http security

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -9,10 +10,16 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) GLOBAL MIDDLEWARES
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit request from the same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -25,17 +32,25 @@ app.use('/api', limiter);
 //   res.send('Testing rate limit headers');
 // });
 
-app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
-// 3) ROUTES
+// Body parser, reading data from the body into req.body
+app.use(
+  express.json({
+    limit: '10kb'
+  })
+);
 
+// Serving static files
+app.use(express.static(`${__dirname}/public`));
+
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  console.log(req.requestTime);
+  // console.log(req.requestTime);
 
   next();
 });
 
+// 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
